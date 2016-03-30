b041524063c2f7fb9dc30b488d4bb633e6f02244
Moch Zhaid Firdaus <zaid.mzf@gmail.com>
2016-03-30 10:25:38 +0700

    Added files via upload

---
+++ fibo/src/fibo/Fibo.java
@@ -0,0 +1,35 @@
+package fibo;
+
+
+import java.util.Scanner;
+
+public class Fibo {
+
+    public Fibo() {
+    }
+   
+    public static void main(String [] args){
+        int n, m, i;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Masukan Batas Deret Fibbonaci = ");
+        n = input.nextInt();
+   
+           for(i=0; i<=n; i++)
+           {
+          
+        m = hitungFibbo(i);
+        System.out.print(m+" ");
+           } 
+    }
+
+    public static int hitungFibbo(int n){  
+        int chan;
+        if(n==0)
+            return 0;
+        if(n==1)
+            return(1); 
+        else
+            chan = hitungFibbo(n-1) + hitungFibbo(n-2);           
+        return chan;
+    }
+ }
